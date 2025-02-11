b73842cd5657f78aaae1ebad8b2f3afa84dd3c03
sefa oz <sefaoz@karatguc.com>
2025-02-11 15:40:06 +0300

    :(

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -16,6 +16,10 @@ export const updateSession = async (request: NextRequest) => {
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
+        auth: {
+          flowType: "pkce",
+          detectSessionInUrl: true,
+        },
         cookies: {
           get(name: string) {
             return request.cookies.get(name)?.value;
